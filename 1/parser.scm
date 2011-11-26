489896f09e315b0c032cc2ea405bd3683858da39
Vegard Øye <vegard_oye@hotmail.com>
2011-11-26 10:09:58 +0100

    Remove supplimentary functions

--- 1/parser.scm
+++ 1/parser.scm
@@ -1,24 +1,11 @@
 #! /usr/bin/racket
 #lang racket
 
-;; Custom version of foldl.
-(define (reduce fn init args)
-  (if (null? args)
-      init
-      (reduce fn
-              (fn init (car args))
-              (cdr args))))
+;; list version of set-union
+(define (union . sets)
+  (reverse (set->list (apply set-union (map list->set sets)))))
 
-;; Custom version of set-union.
-(define (union set1 set2)
-  (if (null? set1)
-      set2
-      (if (member (car set1) set2)
-          (union (cdr set1) set2)
-          (cons (car set1)
-                (union (cdr set1) set2)))))
-
-(define epsilon list)
+;;; Parser combinators
 
 (define-syntax-rule (terminal X ...)
   (lambda (p)
@@ -29,17 +16,17 @@
 
 (define-syntax-rule (seq A ...)
   (lambda (p)
-    (reduce (lambda (v fn)
-              (reduce union '() (map fn v)))
-            (list p)
-            (list A ...))))
+    (foldl (lambda (fn v)
+             (foldl union '() (map fn v)))
+           (list p)
+           (list A ...))))
 
 (define-syntax-rule (alt A ...)
   (lambda (p)
-    (reduce (lambda (v fn)
-              (union v (fn p)))
-            '()
-            (list A ...))))
+    (foldl (lambda (fn v)
+             (union v (fn p)))
+           '()
+           (list A ...))))
 
 (define-syntax-rule (opt A)
   (alt epsilon A))
@@ -48,7 +35,11 @@
   (alt epsilon
        (seq A (k* A))))
 
-;; determiner
+;;; Parsers
+
+(define epsilon list)
+
+;; article
 (define DET (terminal the a))
 
 ;; noun
@@ -67,3 +58,8 @@
 (define VP
   (alt (seq V NP)
        (seq V S)))
+
+;;; Test
+
+(S '(the professor lectures the student))
+(S '(the student studies the cat))
